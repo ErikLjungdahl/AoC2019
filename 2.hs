e0adb78368746f538e38285ef2b94477c70d1a27
ErikLjungdahl <ekke.ekke@gmail.com>
2019-12-03 15:11:53 +0100

    First part done

---
+++ 2.hs
@@ -0,0 +1,29 @@
+import qualified Data.Text as Text --(map, take, drop, splitAt)
+import Data.List
+run f = do
+    str <- readFile "input2.txt"
+    return f str
+
+f1 str = f 12 2 str
+
+f noun verb str = let ints = toInts str in
+    return . head $ readOp 0 $ (head ints):noun:verb:(drop 3 ints)
+
+f2 str = [f noun verb str | noun <- [0..99], verb <- [0..99]]
+
+type Ptr = Int
+type Op = Int -> Int -> Int
+
+toInts :: String -> [Int]
+toInts s = map (read . Text.unpack) $ Text.splitOn (Text.singleton ',') (Text.pack s)
+
+readOp :: Ptr -> [Int] -> [Int]
+readOp p is =
+    let eval :: Ptr -> Op -> Ptr -> Int
+        eval p1 op p2 = (is !! p1) `op` (is !! p2)
+        putAt :: Int -> Ptr -> [Int]
+        putAt i pr = (take pr is) ++ (i:(drop (pr+1) is))
+    in case splitAt p is of
+        (l,1:p1:p2:pr:_) -> readOp (p+4) (putAt (eval p1 (+) p2) pr)
+        (l,2:p1:p2:pr:_) -> readOp (p+4) (putAt (eval p1 (*) p2) pr)
+        (l,99:_) -> is
