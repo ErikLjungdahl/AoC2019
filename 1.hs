5191fd344b3fdadfffeaf78df0d5afa7849049b1
ErikLjungdahl <ekke.ekke@gmail.com>
2019-12-03 13:59:26 +0100

    Nicer code

--- 1.hs
+++ 1.hs
@@ -1,16 +1,13 @@
 
--- main :: IO Int
-run1 = do
+run :: (Int -> Int) -> IO Int
+run f = do
     str <- readFile "input1.txt"
     return $ sum $ map (f . read) (lines str)
-run2 = do
-    str <- readFile "input1.txt"
-    return $ sum $ map (f2 . read) (lines str)
 
-f :: Int -> Int
-f = (\i -> (i `div` 3) -2)
+f1 :: Int -> Int
+f1 = (\i -> (i `div` 3) -2)
 
 f2 :: Int -> Int
 f2 n | a <= 0    = 0
      | otherwise = a + f2 a
-        where a = f n
+        where a = f1 n
