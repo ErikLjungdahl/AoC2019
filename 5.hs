f31d8823077c45ea36305676624b923a3cc53a37
ErikLjungdahl <ekke.ekke@gmail.com>
2019-12-10 13:27:46 +0100

    Day 5, part a done

--- 5.hs
+++ 5.hs
@@ -13,7 +13,7 @@ f noun verb str = let ints = toInts str in
 
 f2 str = head [100 * noun + verb | noun <- [0..99], verb <- [0..99], (f noun verb str) == 19690720]
 -}
-f5 str = readOp 0 (toInts str, [])
+f5 str = head $ snd $ readOp 0 (toInts str, [])
 
 type Ptr = Int
 type Op = Int -> Int -> Int
@@ -24,28 +24,20 @@ toInts s = map (read . Text.unpack) $ Text.splitOn (Text.singleton ',') (Text.pa
 readOp :: Ptr -> ([Int], [Int]) -> ([Int], [Int])
 readOp p (is,output) =
     let eval :: [Int] -> Ptr -> Op -> Ptr -> Int
-        eval list p1 op p2 | p1 >= 0 && p2 >= 0 = eval' list p1 op p2
-                           | otherwise = 0
-        eval' (0:0:_) p1 op p2 = (is !! p1) `op` (is !! p2)
-        eval' (0:1:_) p1 op p2 = (is !! p1) `op` p2
-        eval' (1:0:_) p1 op p2 = p1         `op` (is !! p2)
-        eval' (1:1:_) p1 op p2 = p1         `op` p2
+        eval (0:0:_) p1 op p2 = (is !! p1) `op` (is !! p2)
+        eval (0:1:_) p1 op p2 = (is !! p1) `op` p2
+        eval (1:0:_) p1 op p2 = p1         `op` (is !! p2)
+        eval (1:1:_) p1 op p2 = p1         `op` p2
         putAt :: Int -> Ptr -> [Int]
         putAt i pr = (take pr is) ++ (i:(drop (pr+1) is))
         (o:os) = drop p is
         modOp i = let i' = i `mod` 10
             in i':modOp (i `div` 10)
-        inp = 1 -- Input is always 1?
+        inp = 1 -- Input is always 1
     in case (modOp o, os) of
-        (1:modes ,p1:p2:pres:_) -> readOp (p+4) ((putAt (eval modes p1 (+) p2) pres),output)
-        (2:modes ,p1:p2:pres:_) -> readOp (p+4) ((putAt (eval modes p1 (*) p2) pres),output)
-        (3:modes ,ps:_ )        -> readOp (p+2) ((putAt inp ps),output)
-        (4:modes ,pr:_)         -> readOp (p+2) (is, (is !!+ pr):output)
+        (1:0:modes ,p1:p2:pres:_) -> readOp (p+4) ((putAt (eval modes p1 (+) p2) pres),output)
+        (2:0:modes ,p1:p2:pres:_) -> readOp (p+4) ((putAt (eval modes p1 (*) p2) pres),output)
+        (3:0:modes ,ps:_ )        -> readOp (p+2) ((putAt inp ps),output)
+        (4:0:modes ,pr:_)         -> readOp (p+2) (is, (is !! pr):output)
 
         (9:9:modes,_) -> (is, output)
-
-(!!+) :: [a] -> Int -> a
-[]     !!+ _ = error "Index to large"
-(x:_)  !!+ 0 = x
-(x:xs) !!+ n | n < 0     = error"Negative index"
-             | otherwise = xs !! (n-1)
