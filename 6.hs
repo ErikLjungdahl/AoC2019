b2379e945fb0612a10baa9d2f0f33644b81fa212
ErikLjungdahl <ekke.ekke@gmail.com>
2019-12-10 22:19:07 +0100

    part 1 done

--- 6.hs
+++ 6.hs
@@ -1,11 +1,17 @@
-import Data.Tree
-
+--import Data.Tree
+import qualified Data.HashMap.Strict as M
 run f = do
     str <- readFile "input6.txt"
     let pairs = map (\line -> ((takeWhile (\c -> c /= ')') line), tail (dropWhile (\c -> c /= ')') line ))) (lines str)
     return $ f pairs
 
-f6_1 pairs = pairs
 
-f1 :: [(String,String)] -> Tree String
-f1 ((a,b):ss) = Node a [Node b []] 
+f6_1 :: [(String,String)] -> Int
+f6_1 list =
+    let mp = M.fromListWith (++) [(k , [v]) | (k, v) <- list]
+        com = "COM"
+        acc :: Int -> String -> Int
+        acc n key = case M.lookup key mp of
+            Just vs -> n + sum (map (acc (n+1)) vs)
+            Nothing -> n
+    in acc 0 com
